0fe0134ee3361703c6f4fa35fe006172a2e79c32
ҶΰÁ¢ <782312713@qq.com>
2020-07-03 13:42:32 +0800

    首页开发

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+      <div v-if="!isActive"><slot name="item-icon"></slot></div>
+      <div v-else><slot name="item-icon-active"></slot></div>
+      <div :style="activeStyle"><slot name="item-text"></slot></div>      
+  </div>
+</template>
+s
+<script>
+export default {
+  name:"TabBarItem",  
+  props:{
+    path:String,
+    activeColor:{
+      type:String,
+      default:'red'
+    }
+  },
+  computed:{
+    isActive(){
+      return this.$route.path.indexOf(this.path) !==-1
+    },
+    activeStyle(){
+      return this.isActive?{color:this.activeColor}:{};
+    }
+  },
+  methods:{
+    itemClick(){
+      this.$router.replace(this.path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tab-bar-item{
+  flex:1;
+  height: 49px;
+  font-size: 16px;
+  text-align: center;
+  
+}
+.tab-bar-item img{
+    width: 24px;
+    height: 24px;
+    vertical-align: middle;
+    margin-top: 3px;
+    
+}
+
+</style> 
